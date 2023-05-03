500e38708d4fc58fc6b436cac3b9812a95ac063f
Phạm Ngọc Cường <handsomevip007@gmail.com>
2023-05-03 09:52:33 +0700

    add news for console poly

---
+++ console-poly/news.js
@@ -0,0 +1,7 @@
+var news = [
+    {
+        "title":"Chính thức chạy thử Console Poly từ kỳ Summer 2023",
+        "dateStart":"2023-05-03 00:00:00",
+        "dateEnd":"2023-05-09 00:00:00"
+    }
+]
